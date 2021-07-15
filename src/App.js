a54df37c25f85dccb38f53437f1a240200a645e0
BritainSmith <41024969+BritainSmith@users.noreply.github.com>
2021-07-15 12:05:27 -0500

    Added json-server db / CRUD methods

--- src/App.js
+++ src/App.js
@@ -3,55 +3,97 @@ import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import Task from './components/Task';
 
 function App() {
   const [showAddTask, setShowAddTask]=useState(true)
-  const [tasks, setTasks]= useState([
-        
-    {
-        id:1,
-        text: 'Doctors Appointment',
-        day:'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id:2,
-        text:'Meeting at School',
-        day:'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id:3,
-        text:'Food Shopping',
-        day:'Feb 5th at 2:30pm',
-        reminder: false
-    },
-   
-    
+  const [tasks, setTasks]= useState([])
 
-])
 
-//Add Task
-const addTask =(task) =>{
+  // get tasks from server GET method
+  useEffect(()=>{
+      const getTasks = async () =>{
+        const tasksFromServer = await fetchTasks()
+        setTasks(tasksFromServer)
+      }
+      getTasks()
+  }, [])
+
+  //Fetch Tasks
+
+  const fetchTasks = async () => {
+    const res = await fetch ('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
+//Add Task from server POST method
+
+const addTask = async (task) =>{
+  const res = await fetch('http://localhost:5000/tasks', 
+{
+  method:'POST',
+  headers: {
+    'Content-type': 'application/json'
+  },
+  body: JSON.stringify(task)
+
+})
+const data = await res.json()
+
+setTasks([...tasks, data])
+
+}
+
+// Fetch TASK singular
+//UPDATE toggle reminder
+
+const fetchTask = async (id) => {
+  const res = await fetch (`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+
+  return data
+}
+
+/*const addTask =(task) =>{
 
   const id= Math.floor(Math.random() * 10000) + 1
 
   const newTask={id, ...task}
   setTasks([...tasks, newTask])
-}
+}*/
 
-//Delete Task
-const deleteTask = (id) => {
+//Delete Task from server DELETE method
+const deleteTask = async(id) => {
+
+  await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
   setTasks(tasks.filter((task)=>task.id !== id))
 }
 
 // Toggle Reminder
-const toggleReminder =  (id) => {
+const toggleReminder =  async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask={...Task, reminder : !taskToToggle.reminder}
+
+  const res= await fetch(`http://localhost:5000/tasks/${id}`,{
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+      
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+  
+
+
   setTasks(
     tasks.map((task) =>
     task.id === id ? {...task, reminder: 
-    !task.reminder} : task
+    data.reminder} : task
     )
   )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,6 +3,7 @@ import {FaTimes} from 'react-icons/fa'
 
 const Task = ({ task, onDelete, onToggle, }) =>{
     return(
+        //onDoubleClick not responding for me for some reason.
         <div className={`task ${task.reminder ? 'reminder': ''}`} onDoubleClick={()=>onToggle(task.id)}>
             <h3>{task.text}{' '}
                 <FaTimes 
